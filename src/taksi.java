84b1c6c48a4ec159de4a0b940def4d740f3c204c
ibrahimbutun <ibrahimbutun@outlook.com>
2023-08-15 14:33:32 +0300

    taksi eklendi

---
+++ src/taksi.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class taksi {
+    public static void main(String[] args) {
+        int km;
+        double tutar, perKm = 2.20, startPrice = 10;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Mesafeyi km olarak gir: ");
+        km = inp.nextInt();
+
+        tutar = km * perKm;
+        tutar += startPrice;
+        tutar = (tutar < 20) ? 20 : tutar;
+        System.out.println("Toplam tutar : " + tutar);
+
+    }
+}
